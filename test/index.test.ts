d6ea27d6188f9a1da3e3434a595e612653c05c48
tpluscode <tpluscode@users.noreply.github.com>
2021-05-09 17:04:42 +0200

    chore: prepare for web app

--- test/index.test.ts
+++
@@ -1,9 +0,0 @@
-import { describe, it } from 'mocha'
-import { expect } from 'chai'
-import index from '..'
-
-describe('index', () => {
-  it('exports foo', () => {
-    expect(index).to.eq('foo')
-  })
-})
